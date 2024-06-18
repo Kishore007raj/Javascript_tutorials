be32fe9974e82a041c0cadb735b548e5c89a1c4b
Kishore007raj <kishore2110raj@gmail.com>
2024-06-18 19:13:46 +0530

    new

--- Basic javascript/hello world.js
+++ Basic javascript/hello world.js
@@ -1,13 +1,33 @@
 //This is an in-line comment.
+//for printing don't use print instead use console.log
 //a hello world funcion using console.log
-
+//Task-1
 console.log("Hello World");
 console.log("Nice to meet you!!")
 
 /*this a multiline comment */
 
+//task-2
 //Variables allow computers to store and manipulate data in a dynamic fashion
 //To create or declare variable use "var" as keyword
 //In JavaScript we end statements with semicolons. 
 
 var myName;
+
+//Task-3-storing values with assignment
+
+//In JavaScript, you can store a value in a variable with the assignment operator (=).
+var hello;
+hello="world";
+console.log(hello) //for printing don't use print instead use console.log
+
+
+//task-4-Assigning the Value of One Variable to Another
+
+//assigning a value of a variable to another variable using (=) operator
+var myvar;
+myvar = 10;
+
+var mynum;
+mynum = myvar //the value assign in myvar will be same as mynum
+console.log(mynum)
