7d9af5e6c1b0c99cecc4eb6dfb94dd1054b9581c
Kishore007raj <kishore2110raj@gmail.com>
2024-06-18 18:49:02 +0530

    ade

--- Basic javascript/hello world.js
+++ Basic javascript/hello world.js
@@ -5,3 +5,9 @@ console.log("Hello World");
 console.log("Nice to meet you!!")
 
 /*this a multiline comment */
+
+//Variables allow computers to store and manipulate data in a dynamic fashion
+//To create or declare variable use "var" as keyword
+//In JavaScript we end statements with semicolons. 
+
+var myName;
