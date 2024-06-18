092c52fec71d242f0cbe4c89578693a2060e0608
Kishore007raj <kishore2110raj@gmail.com>
2024-06-19 03:14:30 +0530

    naruto

--- Basic javascript/hello world.js
+++ Basic javascript/hello world.js
@@ -74,4 +74,7 @@ var thisVariableNameIsSoLong;
 
 //task-9-Explore Differences Between the var and let Keywords
 //One of the biggest problems with declaring variables with the var keyword is that you can easily overwrite variable declarations:
+//, when you use "let" instead of "var" for declaring variable , a variable with the same name can only be declared once.
 
+let catName = "Oliver";
+let catSound = "Meow!";
